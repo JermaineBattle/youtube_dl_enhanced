adc0538d184484d92b07a9fc800f17f92051da85
jermainebattle <jermainebattle91@gmail.com>
2024-12-12 11:59:34 -0800

    "docker_uninstall"

---
+++ docker_uninstall.sh
@@ -0,0 +1,34 @@
+# Stop Colima
+colima stop
+
+# Uninstall Colima
+brew uninstall colima
+
+# Remove configs
+rm -rf ~/.colima
+
+# Stop Docker
+pkill Docker
+
+# Uninstall Docker
+brew uninstall docker
+
+# Uninstall Docker Desktop (safety net)
+brew uninstall --cask docker
+
+# Unlink Docker 
+brew unlink docker
+
+# Remove Docker binaries and related files
+rm -rf /usr/local/bin/docker*
+rm -rf ~/.docker
+rm -rf /var/run/docker.sock
+
+# Remove PATH modifications from .bashrc
+sed -i '' '/export PATH=\$PATH:\/usr\/local\/bin/d' ~/.bashrc
+source ~/.bashrc
+
+# Verify
+docker --version || echo "Docker CLI uninstalled successfully."
+colima status || echo "Colima uninstalled successfully."
+ps aux | grep -i docker || echo "No Docker processes running."
