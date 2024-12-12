20a19b346df2cf90923f3596eed88ab6d1db16e1
jermainebattle <jermainebattle91@gmail.com>
2024-12-12 13:37:51 -0800

    "ffmpeg_bin"

--- ffmpeg_bin.sh
+++ ffmpeg_bin.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download/Unpack ffmpeg/ffprobe
-wget -O .env/bin/ffmpeg.7z https://evermeet.cx/ffmpeg/ffmpeg-118022-gca889b1328.7z
-7z x .env/bin/ffmpeg.7z -o.env
-wget -O .env/bin/ffprobe.7z https://evermeet.cx/ffmpeg/ffprobe-118022-gca889b1328.7z
-7z x .env/bin/ffprobe.7z -o.env
+wget -O .env/ffmpeg.7z https://evermeet.cx/ffmpeg/ffmpeg-118022-gca889b1328.7z
+7z x .env/ffmpeg.7z -o.env/bin/
+wget -O .env/ffprobe.7z https://evermeet.cx/ffmpeg/ffprobe-118022-gca889b1328.7z
+7z x .env/ffprobe.7z -o.env/bin/
 
 # Remove zips
-rm .env/bin/ffmpeg.7z
-rm .env/bin/ffprobe.7z
+rm .env/ffmpeg.7z
+rm .env/ffprobe.7z
